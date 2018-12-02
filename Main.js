b67fec4654296e660db6f8608b10a39de5191c91
Hazrat Ali <ali@triggybet.com>
2018-12-02 14:45:51 +0100

    Added TabBav

--- Main.js
+++ Main.js
@@ -4,71 +4,19 @@ import { Permissions, Notifications } from 'expo';
 import { Ionicons } from '@expo/vector-icons';
 import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
 
+
 import Home from './screens/Home';
 import Settings from './screens/Settings';
 
-const HomeNavigator = createStackNavigator({
-    MyHome: {
-        screen: Home,
-        navigationOptions: {
-            header: null
-        }
-    }
-});
+import { TabNavigator } from 'react-navigation';
+impot { Icon } form 'react-native-elements';
 
-const SettingsNavigator = createStackNavigator({
-    MySettings: {
-        screen: Settings,
-        navigationOptions: {
-            header: null
-        }
-    }
-});
-
-const TabRootNavigator = createBottomTabNavigator({
-    MyHome: {
-        screen: HomeNavigator,
-        navigationOptions: {
-            title: 'Home',
-            tabBarIcon : ( { tintColor}) => {
-                return (<View style={{flex: 1, justifyContent: 'center', paddingTop: 5,}}><Ionicons name="md-home" size={28} color={tintColor}/></View>)
-            }
-        }
+const Tabs = TabNavigator({
+    Home: {
+        screen: Home
     },
-    MySettings : {
-        screen: SettingsNavigator,
-        navigationOptions: {
-            title: 'Settings',
-            tabBarIcon : ( { tintColor}) => {
-                return (<View style={{flex: 1, justifyContent: 'center', paddingTop: 5,}}><Ionicons name="md-home" size={28} color={tintColor}/></View>)
-            }
-        }
-    }
-},
-    {
-        tabBarOptions: {
-            activeTintColor: '#082766',
-            inactiveTintColor: '#8C8C8C',
-            style: {
-                backgroundColor: "white",
-                borderTopColor: "transparent"
-            }
-        },
-    }
-);
-
-const RootNavigator = createStackNavigator({
-    TabRootNavigator: {
-        screen: TabRootNavigator,
-        navigationOptions: {
-            header: null
-        }
-    },
-    TabRootNavigator: {
-        screen: TabRootNavigator,
-        navigationOptions: {
-            header: null
-        }
+    Settings: {
+        screen: Settings
     }
 });
 
@@ -81,8 +29,8 @@ class Main extends React.Component {
     render() {
         return (
             <View>
-                <Text>Header</Text>
-                <RootNavigator />
+                <Text>Header 2</Text>
+                <Tabs />
             </View>
         );
 
