24aea9be12663dfaa3784a132e90d61296cdc47c
Hazrat Ali <ali@triggybet.com>
2018-12-02 21:47:24 +0100

    Added image

--- screens/Home.js
+++ screens/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet, Image } from 'react-native';
 
 export default class Home extends React.Component {
 
@@ -9,7 +9,11 @@ export default class Home extends React.Component {
         return(
             <View style={styles.container}>
                 <Button style={styles.button} title="List" onPress={()=>this.props.navigation.navigate('List')} />
-                <Text>This is the Home Screen</Text>
+                <Text>This is the Home Screen 2</Text>
+                <Image
+                    style={{width: '100%', height:200, resizeMode: 'stretch'}}
+                    source={{uri: 'https://media.licdn.com/dms/image/C4E03AQEiPGGFO6QChw/profile-displayphoto-shrink_200_200/0?e=1549497600&v=beta&t=Nh8aM6p8avbTUgjU37y_C3xuPyKgGpEEn_Ouzu41qFU'}}
+                />
             </View>
         );
     }
@@ -25,5 +29,7 @@ const styles = StyleSheet.create({
     button : {
         width: '30px',
         padding: 5
+    },
+    image: {
     }
 })
